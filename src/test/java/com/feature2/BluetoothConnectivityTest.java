37dd8d69f8816c93b1facf84d3bb15dbe936f978
eq-user <160815718+eq-user@users.noreply.github.com>
2024-03-20 19:22:05 +0530

    Add files via upload
    junits

---
+++ src/test/java/com/feature2/BluetoothConnectivityTest.java
@@ -0,0 +1,13 @@
+package com.feature2;
+
+import org.junit.jupiter.api.Test;
+
+public class BluetoothConnectivityTest {
+
+	@Test
+	public void VerifyEDRBluetooth() 
+	{
+		
+	}
+
+}
---
+++ src/test/java/com/feature2/MobileDataConnectivityTest.java
@@ -0,0 +1,12 @@
+package com.feature2;
+
+import org.junit.jupiter.api.Test;
+
+class MobileDataConnectivityTest {
+
+	@Test
+	void Verify5GConnection() {
+		
+	}
+
+}
