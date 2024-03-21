0b7e0b4f0a214d5a79c2baa89b25de0b140f990d
eq-user <160815718+eq-user@users.noreply.github.com>
2024-03-21 18:51:27 +0530

    Add files via upload

---
+++ src/test/java/com/feature4/MultiWindowManagerTest.java
@@ -0,0 +1,15 @@
+package com.feature4;
+
+import org.junit.jupiter.api.Test;
+
+class MultiWindowManagerTest {
+
+	@Test
+	void VerifyStackedWindows() {
+	}
+
+	@Test
+	void VerifyCascadeWindows() {
+	}
+
+}
