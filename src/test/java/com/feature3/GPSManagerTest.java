d5cc6b91e0254ad716c782ea7ed98246ac096385
eq-user <160815718+eq-user@users.noreply.github.com>
2024-03-21 18:42:55 +0530

    Add files via upload

---
+++ src/test/java/com/feature3/GPSManagerTest.java
@@ -0,0 +1,15 @@
+package com.feature3;
+
+import org.junit.jupiter.api.Test;
+
+class GPSManagerTest {
+
+	@Test
+	void VerifyGPSWithGoogleMap() {
+	}
+
+	@Test
+	void VerifyRealtimeNavigation() {
+	}
+
+}
