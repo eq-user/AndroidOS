84aa5e4b8fd4ce23a5857716c59ea7c59b79b7cd
eq-user <160815718+eq-user@users.noreply.github.com>
2024-03-22 17:50:01 +0530

    test feature 3
    test feature 3

---
+++ src/test/java/com/feature3/FingerprintReaderTest.java
@@ -0,0 +1,15 @@
+package com.feature3;
+
+import org.junit.jupiter.api.Test;
+
+class FingerprintReaderTest {
+
+	@Test
+	void VerifyPhoneUnlock() {
+	}
+
+	@Test
+	void VerifyApplicationUnlock() {
+	}
+
+}
